dd45f850338cacbdee08b2115bd9a12e88955ad0
SusanGithaiga <susan.githaiga@student.moringaschool.com>
2022-12-03 06:22:01 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 //Define the totalBatteries variable.
+// Execute the reduce method on the array batteryBatches.
 const totalBatteries = batteryBatches.reduce(function(accumulator , currentValue)
 {
     // Return the sum of all the assembled batteries.
